4561a61987c580a895fc0969c69663bb136fb81c
Taras B <Taras.Buha@gmail.com>
2018-06-20 23:11:51 +0300

    chat

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { CONFIGURATION } from './shared/app.constants';
 
 @Injectable()
 export class ChatService {
 
-  private appid = '76247eeaa25835db3c854fb894704ddf';
   constructor(private http: Http) { }
 
   getWeather(city: string) {
-    const endpoint = `http://localhost:60268/api/media/WeatherForecasts/?city=${city}`;
+    const endpoint = `${CONFIGURATION.baseUrls.server}/api/media/WeatherForecasts/?city=${city}`;
     return this.http.get(endpoint)//, {search: searchParams})
   }
 }
